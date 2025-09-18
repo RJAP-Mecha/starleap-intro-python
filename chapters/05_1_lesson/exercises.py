8eb68f73abe0eb4a3c42b0e4f4655f0a2c0fa20f
RJAP-Mecha <jasperrjap08@gmail.com>
2025-09-18 13:52:25 +0000

    git

--- chapters/05_1_lesson/exercises.py
+++ chapters/05_1_lesson/exercises.py
@@ -4,9 +4,18 @@
 
 print("********** Ch 5 Exercise 1 **********")
 
+def time_since_epoch():
+    import time
+    t = time.time()
+    print("t = ", t)
+    days = int(t // 60 // 60 // 24)
+    print("days = ", days)
+    remainder = t % (days * 60 * 60 * 24)
+    print("remainder = ", remainder)
 
+time_since_epoch()
+    
 
-print("Ch 5 Exercise 1: Not implemented") # Delete this line when you write your code!
 
 
 
@@ -14,7 +23,6 @@ print("********** Ch 5 Exercise 2 **********")
 
 # Do your work for Excercise 2 here.
 
-print("Ch 5 Exercise 2: Not implemented") # Delete this line when you write your code!
 
 
 
@@ -22,7 +30,7 @@ print("********** Ch 5 Exercise 3 **********")
 
 # Do your work for Exercise 3 here.
 
-print("Ch 5 Exercise 3: Not implemented") # Delete this line when you write your code!
+
 
 
 
@@ -30,4 +38,3 @@ print("********** Ch 5 Exercise 4 **********")
 
 # Do your work for Exercise 4 here.
 
-print("Ch 5 Exercise 4: Not implemented") # Delete this line when you write your code!
